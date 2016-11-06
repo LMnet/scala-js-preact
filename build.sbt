78bbc81cf3fb24e7e1c90a0fcb696aab4ab18fdc
LMnet <lmnet89@gmail.com>
2016-11-06 15:14:45 +0700

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "scala-js-preact"
+
+version := "0.1.0-SNAPSHOT"
+
+scalaVersion := "2.12.0"
+
+enablePlugins(ScalaJSPlugin)
---
+++ src/main/scala/test/preact/Main.scala
@@ -0,0 +1,9 @@
+package test.preact
+
+import scala.scalajs.js.JSApp
+
+object Main extends JSApp {
+  def main(): Unit = {
+    println("Hello world!")
+  }
+}
